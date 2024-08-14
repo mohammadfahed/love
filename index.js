3509fadd1b9f7b6526025566213087046906362e
mohammadfahed <152049314+mohammadfahed@users.noreply.github.com>
2024-08-14 09:46:22 -0700

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const target = document.getElementById('target');
     function moveTarget() {
         const maxWidth =200;
-        const maxHeight = 300;
+        const maxHeight = 200;
     
         const randomX = Math.floor(Math.random() * maxWidth);
         const randomY = Math.floor(Math.random() * maxHeight);
